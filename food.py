d8d278a54fa20b8ac534ef7c3ff8ab2d9bccdaad
Siddhant <smulkikar@gmail.com>
2023-12-26 01:38:57 +0530

    decluttered the code a bit

--- food.py
+++ food.py
@@ -1,21 +1,3 @@
-# def check_food(snake, food):
-#     if abs(snake.xcor() - food.xcor()) < 10 and abs(snake.ycor() - food.ycor()) < 10:
-#         return True
-#     else:
-#         return False
-
-# def create_food():
-#     food = Turtle()
-#     food.shape("circle")
-#     food.color("red")
-#     food.shapesize(0.5, 0.5)
-#     x = random.randint(0, 230)
-#     y = random.randint(0, 230)
-#     food.penup()
-#     food.speed("fastest")
-#     food.goto(x, 0)
-#     return food
-
 import random
 from turtle import Turtle
 
